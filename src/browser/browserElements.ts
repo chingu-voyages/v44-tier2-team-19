f8165e89c450ba60ab7d23bf4c18ab6b5b62ab6f
crisxh <76125739+crisxh@users.noreply.github.com>
2023-05-19 19:33:28 +0200

    added sprites and background, updated css with background and placeholder elements

--- src/browser/browserElements.ts
+++ src/browser/browserElements.ts
@@ -2,8 +2,8 @@ const canvas = document.getElementById('scene') as HTMLCanvasElement
 const canvasContext = canvas.getContext('2d') as CanvasRenderingContext2D
 const btnSquare = document.getElementById('addSquare') as HTMLButtonElement
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = 500
+canvas.height = 600
 
 export {
   canvas,
