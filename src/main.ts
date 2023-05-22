74d4ff89e645b6106898cfc4fcb1addb14949ef5
crisxh <76125739+crisxh@users.noreply.github.com>
2023-05-22 15:34:17 +0200

    imported bg main.ts moved assets folder

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import {
   circleCollideWithReactangle,
   getRandomSpeed
 } from './utils/collitions'
-import background from './assets/background/technology-neon-background-concept-free-vector.jpg'
+import 'public/assets/background/technology-neon-background-concept-free-vector.jpg'
 
 /* ============= constants ============= */
 
