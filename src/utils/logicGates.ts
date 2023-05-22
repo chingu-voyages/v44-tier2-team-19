022b1cef395cf8109722409dd02d92754be2b1d2
crisxh <76125739+crisxh@users.noreply.github.com>
2023-05-22 13:22:04 +0200

    scafholding function for winner bot

--- src/utils/logicGates.ts
+++ src/utils/logicGates.ts
@@ -1,5 +1,13 @@
+function and(a,b) {
+  if (a && b) {
+    return true
+  }
+}
+
+
+
 function winOrLose(bot1,bot2,gate): string{
-    if(bot1 && bot2){
+    if(and(bot1,bot2)){
         return 'Tie';
     }
     }
